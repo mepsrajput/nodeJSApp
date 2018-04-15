609044e0ef39dac4446bdf5ad0d8b7c97a3e7e30
Pradeep Singh <viveksingh12126@gmail.com>
2018-04-15 13:37:08 +0530

    index.js updated as per heroku

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+//views is the directory for all template fileSize
+app.set('views',_dirname + '/views');
 app.set('view engine','ejs');
 
 /* GET request on root */
 app.get('/', function(req, res, next) {
-  res.render('index', { title: 'Udacity Student Alumni Network' });
+	res.render('index', { title: 'Udacity Student Alumni Network' });
 });
 
 /* GET request on any other node other than above nodes */
 app.get('*', function(req, res) {
-  res.send("Oops this page doesn't exist. Contact the admin");
+	res.send("Oops this page doesn't exist. Contact the admin");
 });
 
-app.listen(3000,function(){
-console.log("app started");
-
+app.listen(app.get('port'), function(){
+	console.log('app is running on port', app.get('port'));
 });
